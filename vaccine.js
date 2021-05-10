7f4d6c7228df9e2344115bb5fb9535e6e20bc79f
Pankaj Doharey <pankaj.doharey@bigbinary.com>
2021-05-10 08:12:29 +0530

    Takes User Input

--- vaccine.js
+++ vaccine.js
@@ -2,6 +2,7 @@ const notifier = require('node-notifier');
 const path = require('path');
 const https = require('https')
 const open = require('child_process')
+const readline = require("readline-sync");
 
 let notify_availability = () => {
   notifier.notify({
@@ -21,11 +22,14 @@ let padding = (str,size) => {
   return str;
 }
 
-const pin = '382424'
+process.stdout.write("Enter PIN : ") 
+let pin = Number(readline.question())
+process.stdout.write("Enter Minimum Age limit 18/45 : ") 
+let age = Number(readline.question())
 let date = new Date()
 let current_date = padding(date.getDate()) + '-' + padding(date.getMonth() + 1) + '-' + padding(date.getFullYear())
 let url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode=${pin}&date=${current_date}`
-console.log("Trying URL: " , url)
+console.log("\nRequest URL: " , url, "\n")
 
 let check_availability = () => {
   https.get(url, (resp) => {
@@ -38,16 +42,23 @@ let check_availability = () => {
     resp.on('end', () => {
       try {
         let json = JSON.parse(data)
+        let longest = 0
         json["centers"].forEach( (elem) => {
           let capacity = elem["sessions"][0]["available_capacity"]
           let min_age_limit = elem["sessions"][0]["min_age_limit"]
-          if(capacity > 0 && min_age_limit == 18){
+          let name = elem["name"]
+          if(capacity > 0 && min_age_limit == age){
             notify_availability()
           }else{
-            console.log("Nothing Yet: ", capacity, min_age_limit);
-            console.log("--------------------------------------");
+            let msg = "Availability, Age: "+  capacity + ", " + min_age_limit +  " | Hospital : " + name
+            if(msg.length > longest)
+              longest = msg.length
+            console.log(msg)
           }
+
+          console.log(Array(longest+2).join('-'))
         })
+        console.log("\n")
       }catch(err){
         console.error(err)
       }
